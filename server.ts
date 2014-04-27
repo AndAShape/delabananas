be45e1700c8edf5ea36d307da92711f9d54f9e1f
FruitAndAShape <ianwarburton@fruitandashape.com>
2014-04-27 19:21:14 +0100

    Home page being served by node.

---
+++ server.ts
@@ -0,0 +1,58 @@
+/// <reference path="node_modules/typescript-require/typings/node.d.ts" />
+
+var http  = require('http');
+var fs    = require('fs');
+var path  = require('path');
+var mime  = require('mime');
+var cache = {};
+
+var server = http.createServer(function(request, response) {
+	var filePath = '';
+
+	if (request.url == '/') {
+		filePath = 'public/index.htm';
+	} else {
+		filePath = 'public' + request.url;
+	}
+
+	var absPath = './' + filePath;
+
+	serveStatic(response, cache, absPath);
+});
+
+function serveStatic(response, cache, absPath) {
+
+    	fs.exists(absPath, function(exists) {
+
+    		if (exists) {
+
+				fs.readFile(absPath, function(err, data) {
+
+				  if (err) {
+				    send404(response);
+				  } else {
+				    sendFile(response, absPath, data);
+				  }
+
+				});
+
+			} else {
+				send404(response);
+	      	}
+		});
+}
+
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+server.listen(3000, function() {
+          console.log("Server listening on port 3000.");
+});
